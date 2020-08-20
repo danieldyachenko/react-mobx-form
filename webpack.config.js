a8bd134925a4fa777b235531c0a0fb482f731129
danieldyachenko <daniildyachenko95@gmail.com>
2020-08-20 23:13:43 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: './src/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+    },
+    devServer: {
+        index: 'index.html',
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        hot: true,
+        open: true,
+        port: 9000,
+    },
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env', 
+                            '@babel/preset-react',
+                            "@babel/preset-typescript"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'public/robots.txt')
+            }],
+        }),
+    ],
+};
