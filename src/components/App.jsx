ed6e904316f0e351d5ee839381493e7a623e9bca
danieldyachenko <daniildyachenko95@gmail.com>
2020-08-29 22:44:18 +0300

    fix favicon.ico

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+import Icon from "../assets/hourglass.svg"
+
+export default function App() {
+    return (
+        <>
+            <img src={Icon} />
+        </>
+    )
+}
